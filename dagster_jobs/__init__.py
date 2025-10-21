47628b3faef39f395aeedf33df8dd44052186ea1
Isaías Rubio <isarubio95@gmail.com>
2025-10-21 12:37:58 +0200

    Remove unused bronze job and sensor from definitions

--- dagster_jobs/__init__.py
+++ dagster_jobs/__init__.py
@@ -1,10 +1,9 @@
 from dagster import Definitions
-from .ingest_bronze_job import ingest_bronze_job, s3_new_objects_sensor_bronze
 from .ingest_silver_job import ingest_silver_job, s3_new_objects_sensor_silver
 from .organize_parquet import organize_parquet, hourly_compaction
 
 defs = Definitions(
-    jobs=[ingest_bronze_job, ingest_silver_job, organize_parquet],
+    jobs=[ingest_silver_job, organize_parquet],
     schedules=[hourly_compaction],
-    sensors=[s3_new_objects_sensor_bronze, s3_new_objects_sensor_silver],
+    sensors=[s3_new_objects_sensor_silver],
 )
